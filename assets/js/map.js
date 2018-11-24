2d3f093a2332adf204e924f72cf3503df0655861
teppei696 <teppei6969@gmail.com>
2018-11-24 13:19:44 +0900

    map.jsの最終版

--- assets/js/map.js
+++ assets/js/map.js
@@ -27,53 +27,56 @@ function getFoods(lat, lng, onSuccess) {
   const request = new XMLHttpRequest();
   request.open("GET", "/api/foods?lat=" + lat + "&lng=" + lng);
   request.addEventListener("load", (event) => {
-    onSuccess(JSON.parse(event.target.responseText));
+    onSuccess(event.target.responseText);
   });
   request.send();
 }
 
 function initMap(lat, lng) {
   getFoods(lat, lng, function (response) {
-    console.log(response);
-  });
-
-  // 地図の作成
-  var mapLatLng = new google.maps.LatLng({
-    lat: markerData[0]['lat'],
-    lng: markerData[0]['lng']
-  }); // 緯度経度のデータ作成
-  map = new google.maps.Map(document.getElementById('sample'), { // #sampleに地図を埋め込む
-    center: {
+    markerData = JSON.parse(response);
+    // 地図の作成
+    var mapLatLng = new google.maps.LatLng({
       lat: lat,
       lng: lng
-    }, // 地図の中心を指定
-    zoom: 13 // 地図のズームを指定
-  });
-  // マーカー毎の処理
-  for (var i = 0; i < markerData.length; i++) {
-    markerLatLng = new google.maps.LatLng({
-      lat: markerData[i]['lat'],
-      lng: markerData[i]['lng']
     }); // 緯度経度のデータ作成
-    marker[i] = new google.maps.Marker({ // マーカーの追加
-      position: markerLatLng, // マーカーを立てる位置を指定
-      map: map // マーカーを立てる地図を指定
+    map = new google.maps.Map(document.getElementById('sample'), { // #sampleに地図を埋め込む
+      center: {
+        lat: lat,
+        lng: lng
+      }, // 地図の中心を指定
+      zoom: 15 // 地図のズームを指定
     });
-    infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
-      content: '<div class="sample"><img src="' + markerData[i]['img'] + '" alt="" width="100" height="100" border="0" /></div>' // 吹き出しに表示する内容
-    });
-    // Event
-    infoWindow[i].addListener("closeclick", function (argument) {
-      currentInfoWindow = null;
-    });
-    markerEvent(i); // マーカーにクリックイベントを追加
-    marker[i].setOptions({ // TAM 東京のマーカーのオプション設定
-      icon: {
-        url: markerData[i]['img'], // マーカーの画像を変更
-        scaledSize: new google.maps.Size(75, 75)
-      }
-    });
-  }
+    // マーカー毎の処理
+    for (var i = 0; i < markerData.length; i++) {
+      console.log(markerData[i].lat);
+      console.log(markerData[i].lon);
+      console.log(markerData[i].name);
+      markerLatLng = new google.maps.LatLng({
+        lat: parseFloat(markerData[i].lat),
+        lng: parseFloat(markerData[i].lon)
+      }); // 緯度経度のデータ作成
+      marker[i] = new google.maps.Marker({ // マーカーの追加
+        position: markerLatLng, // マーカーを立てる位置を指定
+        map: map // マーカーを立てる地図を指定
+      });
+      infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
+        content: '<div class="sample">' + markerData[i].name + '</div><div><button type="button" name="aaa" value="aaa">Reservation</button></div>' // 吹き出しに表示する内容
+      });
+      // Event
+      infoWindow[i].addListener("closeclick", function (argument) {
+        currentInfoWindow = null;
+      });
+      markerEvent(i); // マーカーにクリックイベントを追加
+      marker[i].setOptions({ // TAM 東京のマーカーのオプション設定
+        icon: {
+          url: markerData[i].image,
+          // url: 'image/img' + (i % 10) + '_1.png',// マーカーの画像を変更
+          scaledSize: new google.maps.Size(75, 75)
+        }
+      });
+    }
+  });
 }
 // マーカーにクリックイベントを追加
 function markerEvent(i) {
@@ -89,7 +92,7 @@ function markerEvent(i) {
 }
 // 成功した時の関数
 function successFunc(position) {
-  initMap(position.coords.latitude, position.coords.longitude);
+  initMap(35.6275767, 135.0608655);
 }
 // 失敗した時の関数
 function errorFunc(error) {
